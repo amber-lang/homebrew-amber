a57427a1400c079d50d842c3c606a217251f9a50
Jihyeon Kim (김지현) <simnalamburt@gmail.com>
2025-06-11 05:37:01 +0000

    amber-lang: add 0.4.0-alpha bottle.

--- Formula/amber-lang.rb
+++ Formula/amber-lang.rb
@@ -6,6 +6,14 @@ class AmberLang < Formula
   license "GPL-3.0-only"
   head "https://github.com/amber-lang/amber.git", branch: "staging"
 
+  bottle do
+    root_url "https://github.com/simnalamburt/homebrew-amber/releases/download/amber-lang-0.4.0-alpha"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0c7fe23bb9802847687f1990bf9cb68163d460cc4faf747dcc5c2028b4ec9c55"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4c1eeca1b580187f8549ba3b97af43f85e99420429e14d914d1885342618c7ba"
+    sha256 cellar: :any_skip_relocation, ventura:       "8ee8560c6358b2effdaa2ec1fdcc9c5e5d3bbc9c04976c0183bdd41ebf28793b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3f570aa5a3a1b89166c2f0fa35a913eed0e8b5b94f9fb3c2d105746ddc902c13"
+  end
+
   depends_on "rust" => :build
 
   depends_on "bash"
